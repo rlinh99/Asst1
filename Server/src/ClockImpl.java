2a78b600a4b994ef33df5fdb67559f6c10c9e250
Sen Lin <rlin.h99@gmail.com>
2019-01-25 10:31:48 -0800

    Added Printing time on Server
    -Added Printing time on server

--- Server/src/ClockImpl.java
+++ Server/src/ClockImpl.java
@@ -11,7 +11,9 @@ import java.time.ZoneId;
 public class ClockImpl implements Clock {
     @Override
     public LocalDateTime getTime() throws RemoteException {
-        return LocalDateTime.now();
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println("The Server Time is: "+ time.toString());
+        return time;
     }
 
     public String getZoneId() throws RemoteException{
